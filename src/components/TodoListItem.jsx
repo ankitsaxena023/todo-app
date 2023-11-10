988b5511b2171565801b302db35743375801c5ed
Ankit Saxena <98516370+ankitsaxena023@users.noreply.github.com>
2023-11-10 22:04:55 +0530

    update input size

--- src/components/TodoListItem.jsx
+++ src/components/TodoListItem.jsx
@@ -10,7 +10,7 @@ const TodoListItem = ({ itemArray, deleteItem, toggleCheckbox, updateItem }) => 
     <>
       <div>
         <input type="checkbox" checked={itemArray.isEditable} onChange={toggleCheckbox} />
-        <input style={itemArray.isEditable ? { textDecoration: 'line-through' } : { textDecoration: 'none' }} type="text" value={listName} onChange={(e) => setListName(e.target.value)} readOnly={itemArray.isUpdatable} />
+        <input style={itemArray.isEditable ? { textDecoration: 'line-through' } : { textDecoration: 'none' }} type="text" value={listName} onChange={(e) => setListName(e.target.value)} readOnly={itemArray.isUpdatable} size="15"/>
         <button disabled={itemArray.isEditable} onClick={updateItem}>{itemArray.isUpdatable ? 'edit' : 'update'}</button>
         <button onClick={deleteItem}>delete</button>
       </div>
